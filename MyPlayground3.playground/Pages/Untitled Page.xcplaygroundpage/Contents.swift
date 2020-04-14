78a2d10ab16d88d29bc9abc6d682a3a8d317c7df
MorganaGalamba <morgana.feijo@gmail.com>
2020-04-14 15:56:09 -0300

    telas escalonadas para mac e teclado melhorado

--- MyPlayground3.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground3.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -15,8 +15,8 @@ class MyViewController : UIViewController {
     
         
         let label1 = UILabel()
-        label1.frame = CGRect(x: 200, y: 210, width: 400, height: 30)
-        label1.font = UIFont(name: "HelveticaNeue", size: 30)
+        label1.frame = CGRect(x: 350, y: 240, width: 745, height: 80)
+        label1.font = UIFont(name: "HelveticaNeue", size: 50)
         label1.text = "Olá, que legal ter você aqui!"
         label1.textColor = .black
         
@@ -28,13 +28,13 @@ class MyViewController : UIViewController {
         }*/
         
         let label2 = UILabel()
-        label2.frame = CGRect(x: 200, y: 240, width: 400, height: 30)
-        label2.font = UIFont(name: "HelveticaNeue", size: 30)
+        label2.frame = CGRect(x: 350, y: 320, width: 500, height: 70)
+        label2.font = UIFont(name: "HelveticaNeue", size: 50)
         label2.text = "Qual o seu nome?"
         label2.textColor = .black
         
         let okButton = UIButton()
-        okButton.frame = CGRect(x: 550, y: 500, width: 50, height: 31)
+        okButton.frame = CGRect(x: 1050, y: 640, width: 91, height: 72)
         okButton.setTitle("ok", for: .normal)
         okButton.setTitleColor(.white, for: .normal)
         okButton.backgroundColor = .myRed
@@ -43,14 +43,14 @@ class MyViewController : UIViewController {
         okButton.addTarget(nil, action: #selector(tapButton), for: .touchUpInside)
         
         let textField = UITextField()
-        textField.frame = CGRect(x: 200, y: 500, width: 320, height: 30)
+        textField.frame = CGRect(x: 290, y: 640, width: 719, height: 75)
         textField.backgroundColor = .white
         textField.layer.masksToBounds = true
         textField.layer.cornerRadius = 2
         
         let baloom = UIImage(named: "Group 5" )
         let baloomImageView = UIImageView(image: baloom)
-        baloomImageView.frame = CGRect(x: 100, y: 150, width: 600, height: 200)
+        baloomImageView.frame = CGRect(x: 225, y: 150, width: 990, height: 350)
         
         view.addSubview(baloomImageView)
         view.addSubview(label1)
@@ -62,31 +62,33 @@ class MyViewController : UIViewController {
     
     @objc func tapButton(){
         print("apertou ok")
-        present(SecondViewController,animated: false,completion: nil)
+        navigationController?.pushViewController(secondViewController, animated: false)
     }
     
     
 }
 
-class secondViewController : UIViewController {
+class SecondViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .myWhite
 
         let label1 = UILabel()
-        label1.frame = CGRect(x: 200, y: 210, width: 400, height: 30)
-        label1.font = UIFont(name: "HelveticaNeue", size: 26)
+        label1.frame = CGRect(x: 290, y: 210, width: 860, height: 80)
+        label1.font = UIFont(name: "HelveticaNeue", size: 60)
         label1.text = "Adoramos o seu nome, Lucas!"
         label1.textColor = .black
         
         let label2 = UILabel()
-        label2.frame = CGRect(x: 200, y: 240, width: 400, height: 30)
-        label2.font = UIFont(name: "HelveticaNeue", size: 26)
+        label2.frame = CGRect(x: 290, y: 270, width: 860, height: 230)
+        label2.font = UIFont(name: "HelveticaNeue", size: 60)
         label2.text = "Agora queremos saber com quem você quer brincar de dançar?"
         label2.textColor = .black
+        label2.lineBreakMode = .byWordWrapping
+        label2.numberOfLines = 3
         
         let giButton = UIButton()
-        giButton.frame = CGRect(x: 275, y: 500, width: 100, height: 40)
+        giButton.frame = CGRect(x: 416, y: 700, width: 279, height: 78)
         giButton.setTitle("Gi", for: .normal)
         giButton.setTitleColor(.white, for: .normal)
         giButton.backgroundColor = .myRed
@@ -95,7 +97,7 @@ class secondViewController : UIViewController {
         giButton.addTarget(nil, action: #selector(tapButton), for: .touchUpInside)
         
         let dedeButton = UIButton()
-        dedeButton.frame = CGRect(x: 425, y: 500, width: 100, height: 40)
+        dedeButton.frame = CGRect(x: 745, y: 700, width: 279, height: 78)
         dedeButton.setTitle("Dede", for: .normal)
         dedeButton.setTitleColor(.white, for: .normal)
         dedeButton.backgroundColor = .myRed
@@ -105,7 +107,7 @@ class secondViewController : UIViewController {
         
         let baloom = UIImage(named: "Group 5" )
         let baloomImageView = UIImageView(image: baloom)
-        baloomImageView.frame = CGRect(x: 100, y: 100, width: 600, height: 300)
+        baloomImageView.frame = CGRect(x: 100, y: 100, width: 1300, height: 500)
         
         view.addSubview(baloomImageView)
         view.addSubview(label1)
@@ -117,28 +119,28 @@ class secondViewController : UIViewController {
     
     @objc func tapButton(){
         print("apertou gi ou dede")
-        present(ThirdViewController,animated: false,completion: nil)
+        navigationController?.pushViewController(thirdViewController, animated: false)
     }
 }
 
-class thirdViewController : UIViewController {
+class ThirdViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .myWhite
         
         let groundBox = UIView()
-        groundBox.frame = CGRect(x: 0, y: 350, width: 800, height: 300)
+        groundBox.frame = CGRect(x: 0, y: 550, width: 1440, height: 350)
         groundBox.backgroundColor = .myGround
         
         let actionBox = UILabel()
-        actionBox.frame = CGRect(x: 350, y:15 , width: 100, height: 20)
+        actionBox.frame = CGRect(x: 631, y:15 , width: 178, height: 33)
         actionBox.backgroundColor = .myYellow
         actionBox.text = "Pular"
-        actionBox.font = UIFont(name: "HelveticaNeue", size: 16)
+        actionBox.font = UIFont(name: "HelveticaNeue", size: 20)
         actionBox.textAlignment = .center
         
         let danceButton = UIButton()
-        danceButton.frame = CGRect(x: 325, y: 450, width: 150, height: 30)
+        danceButton.frame = CGRect(x: 561, y: 730, width: 318, height: 60)
         danceButton.setTitle("Entendi", for: .normal)
         danceButton.setTitleColor(.white, for: .normal)
         danceButton.addTarget(nil, action: #selector(tapButton), for: .touchUpInside)
@@ -155,62 +157,64 @@ class thirdViewController : UIViewController {
     
     @objc func tapButton(){
         print("apertou entendi")
-        present(MainViewController,animated: false,completion: nil)
+        navigationController?.pushViewController(mainViewController, animated: false)
     }
 }
-class mainViewController : UIViewController {
+class MainViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .myWhite
         
         let groundBox = UIView()
-        groundBox.frame = CGRect(x: 0, y: 350, width: 800, height: 300)
+        groundBox.frame = CGRect(x: 0, y: 550, width: 1440, height: 350)
         groundBox.backgroundColor = .myGround
         
         let instructionsBox = UIView()
-        instructionsBox.frame = CGRect(x: -10, y: 150, width: 210, height: 300)
+        instructionsBox.frame = CGRect(x: -10, y: 150, width: 297, height: 500)
         instructionsBox.backgroundColor = .myYellow
         instructionsBox.layer.masksToBounds = true
         instructionsBox.layer.cornerRadius = 10
         
         let howToLabel = UILabel()
         howToLabel.text = "Como Fazer"
-        howToLabel.font =  UIFont(name: "HelveticaNeue-Bold", size: 21)
-        howToLabel.frame = CGRect(x: 26, y: 170, width: 150, height: 30)
+        howToLabel.font =  UIFont(name: "HelveticaNeue-Bold", size: 25)
+        howToLabel.frame = CGRect(x: 65, y: 170, width: 156, height: 35)
         
         let instruction1 = UILabel()
-        instruction1.frame = CGRect(x: 10, y: 200, width: 150, height: 30)
-        instruction1.text = "1.Dobre um poucos dois joelhos"
-        instruction1.font = UIFont(name:"HelveticaNeue", size:18)
+        instruction1.frame = CGRect(x: 10, y: 210, width: 250, height: 80)
+        instruction1.text = "1.Dobre um poucos os dois joelhos"
+        instruction1.font = UIFont(name:"HelveticaNeue", size:20)
+        instruction1.lineBreakMode = .byWordWrapping
+        instruction1.numberOfLines = 2
         
         let instruction2 = UILabel()
-        instruction2.frame = CGRect(x: 10, y: 220, width: 150, height: 30)
+        instruction2.frame = CGRect(x: 10, y: 280, width: 150, height: 40)
         instruction2.text = "2.Pule"
-        instruction2.font = UIFont(name:"HelveticaNeue", size:18)
+        instruction2.font = UIFont(name:"HelveticaNeue", size:20)
         
-        let menuButton = UIButton()
+        /*let menuButton = UIButton()
         menuButton.frame = CGRect(x: 10, y: 10, width: 30, height:30 )
         menuButton.backgroundColor = .myRed
         menuButton.layer.masksToBounds = true
-        menuButton.layer.cornerRadius = 5
+        menuButton.layer.cornerRadius = 5*/
         
-        let actionBox = UILabel()
-        actionBox.frame = CGRect(x: 350, y:15 , width: 100, height: 20)
+       let actionBox = UILabel()
+        actionBox.frame = CGRect(x: 631, y:15 , width: 178, height: 33)
         actionBox.backgroundColor = .myYellow
         actionBox.text = "Pular"
-        actionBox.font = UIFont(name: "HelveticaNeue", size: 16)
+        actionBox.font = UIFont(name: "HelveticaNeue", size: 20)
         actionBox.textAlignment = .center
         
         let danceButton = UIButton()
-        danceButton.frame = CGRect(x: 325, y: 450, width: 150, height: 30)
-        danceButton.setTitle("Dança de novo, G", for: .normal)
+        danceButton.frame = CGRect(x: 561, y: 670, width: 318, height: 60)
+        danceButton.setTitle("Dança de novo, Gi", for: .normal)
         danceButton.setTitleColor(.white, for: .normal)
         danceButton.backgroundColor = .myRed
         danceButton.layer.masksToBounds = true
         danceButton.layer.cornerRadius = 5
         
         let hardButton = UIButton()
-        hardButton.frame = CGRect(x: 240, y: 500, width: 150, height: 30)
+        hardButton.frame = CGRect(x: 397, y: 740, width: 318, height: 60)
         hardButton.setTitle("Esta muito difícil", for: .normal)
         hardButton.setTitleColor(.white, for: .normal)
         hardButton.backgroundColor = .myRed
@@ -218,14 +222,14 @@ class mainViewController : UIViewController {
         hardButton.layer.cornerRadius = 5
         
         let gotItButton = UIButton()
-        gotItButton.frame = CGRect(x: 410, y: 500, width: 150, height: 30)
+        gotItButton.frame = CGRect(x: 725, y: 740, width: 318, height: 60)
         gotItButton.setTitle("Consegui", for: .normal)
         gotItButton.setTitleColor(.white, for: .normal)
         gotItButton.backgroundColor = .myRed
         gotItButton.layer.masksToBounds = true
         gotItButton.layer.cornerRadius = 5
         
-        view.addSubview(menuButton)
+        //view.addSubview(menuButton)
         view.addSubview(actionBox)
         view.addSubview(groundBox)
         view.addSubview(danceButton)
@@ -240,21 +244,18 @@ class mainViewController : UIViewController {
     }
 }
 
-let MainViewController = mainViewController()
-let SecondViewController = secondViewController()
-let ThirdViewController = thirdViewController()
+let navigation = UINavigationController(screenType: .mac)
+
+let mainViewController = MainViewController()
+let secondViewController = SecondViewController()
+let thirdViewController = ThirdViewController()
 let vc = MyViewController()
 
-vc.preferredContentSize = CGSize(width: 800, height: 600)
-MainViewController.preferredContentSize = CGSize(width: 800, height: 600)
-SecondViewController.preferredContentSize = CGSize(width: 800, height: 600)
-ThirdViewController.preferredContentSize = CGSize(width: 800, height: 600)
-ThirdViewController.modalPresentationStyle = .fullScreen
-MainViewController.modalPresentationStyle = .fullScreen
-SecondViewController.modalPresentationStyle = .fullScreen
+navigation.pushViewController(vc, animated: false)
+navigation.navigationBar.isHidden = true
 
 PlaygroundPage.current.needsIndefiniteExecution = true // Sets up the constant running of the playground so that the location can update properly
 
-PlaygroundPage.current.liveView = vc
+PlaygroundPage.current.liveView = navigation.scale(to:0.5)
 
 
